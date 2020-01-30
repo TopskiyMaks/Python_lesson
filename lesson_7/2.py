a47fa5a7c18479c49bc973b7311d0304b1677ea3
TopskiyMaks <jombo59rus@gmail.com>
2020-01-30 18:41:24 +0500

    Выполнил задания к 7му уроку

---
+++ lesson_7/2.py
@@ -0,0 +1,35 @@
+# 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив, заданный случайными числами на
+# промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+
+from random import random
+
+
+def merge_sort(a):
+    if len(a) < 2:
+        return a
+    left = merge_sort(a[:len(a) // 2])
+    right = merge_sort(a[len(a) // 2:len(a)])
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            a[k] = left[i]
+            i = i + 1
+        else:
+            a[k] = right[j]
+            j = j + 1
+        k = k + 1
+    while i < len(left):
+        a[k] = left[i]
+        i = i + 1
+        k = k + 1
+    while j < len(right):
+        a[k] = right[j]
+        j = j + 1
+        k = k + 1
+    return a
+
+
+if __name__ == '__main__':
+    arr = [int(random() * 100) for i in range(10)]
+    print(arr)
+    print(merge_sort(arr))
